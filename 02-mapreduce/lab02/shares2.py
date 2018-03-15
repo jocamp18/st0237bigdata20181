a3ffafd8dd376cab91c13fe3f393a214f0dde726
jocamp18 <jocamp18@eafit.edu.co>
2018-03-15 08:16:16 -0500

    Adding some data to stock exercise

--- 02-mapreduce/lab02/shares2.py
+++ 02-mapreduce/lab02/shares2.py
@@ -10,7 +10,7 @@ class MRShares2(MRJob):
 	def reducer(self, share, value_date):
 		value_date_list = list(value_date)
 		value_sort = sorted(value_date_list, key=itemgetter(0))
-		date_sort = sorted(value_date_list, key=itemgetter(0))
+		date_sort = sorted(value_date_list, key=itemgetter(1))
 		if value_sort == date_sort and value_sort[len(value_sort)-1][1] > value_sort[0][1]:
 			yield share, "True"
 
--- 02-mapreduce/lab02/shares3.py
+++ 02-mapreduce/lab02/shares3.py
@@ -5,4 +5,21 @@ class MRShares3(MRJob):
 	
 	def mapper(self, _, line):
 		share, value, date = line.split(',')
-		yield share
+		yield share, (float(value), date)
+
+	def reducer1(self, share, value_date):
+		yield 1, min(value_date)[1]
+
+	def reducer2(self, _, date):
+		date = list(date)
+		yield 1, max(date, key=date.count)
+
+	def steps(self):
+		return[
+			MRStep(mapper = self.mapper, reducer = self.reducer1),
+			MRStep(reducer = self.reducer2)
+		]
+
+if __name__ == '__main__':
+	MRShares3.run()
+
